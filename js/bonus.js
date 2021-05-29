f0e669a42c6318073472454e30096014cfd3c6b5
MattiaR <rovitomattia91@gmail.com>
2021-05-29 17:35:30 +0200

    add bonus

---
+++ js/bonus.js
@@ -0,0 +1,9 @@
+// stampare nella console tutti i numero da 10 a 30 (compresi) che sono multipli di 3.
+
+
+
+for (var i = 10; i <= 30; i++) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
